4569f3b4c89ff0a5635f764193d68ad1d428430f
deelaksha <deelakshadeekshith@gmail.com>
2025-02-20 21:46:00 +0530

    java commit

--- java/Polymorphism.java
+++ java/Polymorphism.java
@@ -1,9 +1,19 @@
-
+class Human{
+    String name;
+    int age;
+    public void display_name(String name,int age){
+        this.name = name;
+        this.age = age;
+        System.out.println(name+" "+age);
+    }
+}
 
 public class Polymorphism {
     public static void main(String args[]) {
-        System.out.println("hello world");
-
+        Human human = new Human();
+        human.display_name("harshith", 20);
+          
         // Creating an Animal object
+   System.out.println("heloo worlld");
     }
 }
